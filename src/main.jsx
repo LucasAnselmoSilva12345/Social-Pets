f8487080e0ecc60976c7acda8c3dd8a91fa96096
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-17 15:17:38 -0300

    Feat Styles about the input form, text Feat imports functions
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './styles/global.css';
+import './styles/form.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
