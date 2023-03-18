637e03eaa87ad1c7bcfc9b7621ffb46f590d276f
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 12:57:27 -0300

    Feat Header component with content and styles Fixes styles
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/1
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { ReactComponent as Dogs } from '../../assets/dogs.svg';
+
 import style from './style.module.css';
 
 export function Header() {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={style.header}>
+      <nav className={`${style.nav} container`}>
+        <Link to="/" aria-label="Dogs - Home" className={style.logoImg}>
+          <Dogs />
+        </Link>
+        <Link to="/login" className={style.login}>
+          Login / Criar
+        </Link>
       </nav>
     </header>
   );
