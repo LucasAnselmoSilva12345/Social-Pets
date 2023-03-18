7a6c2f1c0772606739b8955ec9a4894fac1a5e10
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 14:46:22 -0300

    Feat components above process login
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/2
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/components/LoginCreateUser/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function LoginCreateUser() {
+  return (
+    <div>
+      <h2>Login criar</h2>
+    </div>
+  );
+}
---
+++ src/components/LoginPasswordLost/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function LoginPasswordLost() {
+  return <div>index</div>;
+}
