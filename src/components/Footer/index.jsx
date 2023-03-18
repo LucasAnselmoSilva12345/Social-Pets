f65b1d33883818c35f5552cf63f2a85b3e30335a
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 12:04:41 -0300

    Feat Footer component
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './style.module.css';
+
+export function Footer() {
+  return (
+    <footer>
+      <p>© 2020 - My Company</p>
+    </footer>
+  );
+}
