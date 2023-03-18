e2d49c4d53ab6843ec979e699a65860e2227f6a6
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 12:06:46 -0300

    Feat colors variables Feat imports RoutesApp
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,5 @@
+import { RoutesApp } from './routes';
+
 export function App() {
-  return (
-    <div>
-      <h1>Social Pets</h1>
-      <p>
-        Social Pets is a popular website that serves as a hub for pet owners and
-        animal lovers alike. The website provides a wealth of information on
-        various types of pets, including cats, dogs, birds, and reptiles, among
-        others. One of the main features of Social Pets is its community of pet
-        owners who share their experiences, tips, and advice on how to care for
-        their furry, feathered, or scaled companions. The website offers a
-        platform for pet owners to connect with each other, share stories, and
-        ask for help when needed. Social Pets also features a comprehensive
-        directory of pet-related businesses and services, including
-        veterinarians, groomers, trainers, and pet stores. The website offers
-        reviews and ratings from other pet owners, making it easier to find the
-        right service for your pet's needs. Another great feature of Social Pets
-        is its extensive library of articles and guides on pet care, health, and
-        behavior. Whether you're a new pet owner or a seasoned pro, the website
-        has something to offer everyone. In addition to its wealth of
-        information and resources, Social Pets also offers a range of fun and
-        interactive features, such as a pet photo gallery, quizzes, and
-        contests. The website is also home to a lively forum where pet owners
-        can discuss a wide range of topics and connect with other animal lovers
-        from around the world. Overall, Social Pets is a fantastic resource for
-        anyone who loves animals and wants to learn more about how to care for
-        them. With its comprehensive information, engaging community, and fun
-        features, it's easy to see why the website is such a popular destination
-        for pet owners and animal enthusiasts alike.
-      </p>
-    </div>
-  );
+  return <RoutesApp />;
 }
