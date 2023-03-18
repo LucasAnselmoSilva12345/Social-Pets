016f2b0395cdd981f8f79f511141384537adf093
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 16:54:56 -0300

    Feat refactoring Use Form Hook
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/7
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,15 +1,58 @@
 import { useState } from 'react';
 
-export const useForm = () => {
-  const [value, setValue] = useState('');
+const VALIDATION_RULES = {
+  email: {
+    regex: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+    message: 'Please enter a valid email address',
+  },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+    message:
+      'A senha deve ter 8 caracteres, uma letra maiúscula, uma minúscula e um número',
+  },
+};
 
-  function onChange({ target }) {
-    setValue(target.value);
-  }
+export const useForm = (inputType) => {
+  const [value, setValue] = useState('');
+  const [error, setError] = useState(null);
+
+  const validate = (inputValue) => {
+    if (!inputValue || !VALIDATION_RULES[inputType]) {
+      return true;
+    }
+
+    if (inputValue.length === 0) {
+      setError('Please, fill in the field');
+      return false;
+    }
+
+    if (!VALIDATION_RULES[inputType].regex.test(inputValue)) {
+      setError(VALIDATION_RULES[inputType].message);
+      return false;
+    }
+
+    setError(null);
+    return true;
+  };
+
+  const onChange = ({ target }) => {
+    const inputValue = target.value;
+    if (error) {
+      validate(inputValue);
+    }
+    setValue(inputValue);
+  };
+
+  const onBlur = () => {
+    validate(value);
+  };
 
   return {
     value,
     setValue,
     onChange,
+    error,
+    validate,
+    onBlur,
   };
 };
