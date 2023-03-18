99f1ceda41e159942cde463684348056deea5883
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 17:23:51 -0300

    Feat save token in window local storage Feat checks if there is already authenticate user
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/8
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { TOKEN_POST } from '../../api/api.js';
+import { TOKEN_POST, USER_GET } from '../../api/api.js';
 import { useForm } from '../../hooks/useForm';
 import { Button } from '../Forms/Button';
 import { Input } from '../Forms/Input/index';
 
 export function LoginForm() {
   const username = useForm();
-  const password = useForm('password');
+  const password = useForm();
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token');
+    if (token) {
+      getUser(token);
+    }
+  }, []);
+
+  async function getUser(token) {
+    const { url, options } = USER_GET(token);
+    const response = await fetch(url, options);
+    const json = await response.json();
+  }
 
   async function handleAuthenticateUser(event) {
     event.preventDefault();
@@ -20,6 +33,7 @@ export function LoginForm() {
 
       const response = await fetch(url, options);
       const json = await response.json();
+      window.localStorage.setItem('token', json.token);
     }
   }
 
