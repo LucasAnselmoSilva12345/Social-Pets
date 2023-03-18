0c45de32b9b76067a1d7ac3baf8550533ac5d119
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 12:05:34 -0300

    Feat Routes components
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/routes/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Login } from '../components/Login';
+import { Home } from '../pages/Home';
+
+export function RoutesApp() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  );
+}
