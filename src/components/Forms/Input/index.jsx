b30e04d27409778019a561d0b3b64a2ac1f3e74b
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-18 15:33:26 -0300

    Feat Login page with inputs, buttons and styles
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/3
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Forms/Input/index.jsx
+++ src/components/Forms/Input/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import style from './style.module.css';
 
-export function Input({ label, type, id }) {
+export function Input({ id, label, type }) {
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
+    <div className={style.wrapper}>
+      <label htmlFor={id} className={style.label}>
+        {label}
+      </label>
       <input id={id} name={id} className={style.input} type={type} />
     </div>
   );
