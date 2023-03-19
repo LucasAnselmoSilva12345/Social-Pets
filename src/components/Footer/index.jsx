4cf6937fb9eed16bc54a3b5fb455cf8db173e692
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-03-19 19:59:21 -0300

    Feat Footer component
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/11
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import { ReactComponent as DogsFooter } from '../../assets/dogs.svg';
+
 import style from './style.module.css';
 
 export function Footer() {
   return (
-    <footer>
-      <p>© 2020 - My Company</p>
+    <footer className={style.footer}>
+      <DogsFooter />
+      <p>Social Pets. All rights reserved</p>
     </footer>
   );
 }
