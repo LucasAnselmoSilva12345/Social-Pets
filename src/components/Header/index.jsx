6ec80acfce7e21b72eb7d659e280716fd846dbc3
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-04-01 17:06:25 -0300

    Feat User login and show the result of header with data user authenticate
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/15
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-
 import { ReactComponent as Dogs } from '../../assets/dogs.svg';
+import { UserContext } from '../../contexts/UserContext';
 
 import style from './style.module.css';
 
 export function Header() {
+  const { dataUser } = useContext(UserContext);
+
   return (
     <header className={style.header}>
       <nav className={`${style.nav} container`}>
         <Link to="/" aria-label="Dogs - Home" className={style.logoImg}>
           <Dogs />
         </Link>
-        <Link to="/login" className={style.login}>
-          Login / Criar
-        </Link>
+
+        {dataUser ? (
+          <Link to="/account" className={style.login}>
+            {dataUser.nome}
+          </Link>
+        ) : (
+          <Link to="/login" className={style.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,39 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { TOKEN_POST, USER_GET } from '../../api/api.js';
 import { useForm } from '../../hooks/useForm';
 import { Button } from '../Forms/Button';
 import { Input } from '../Forms/Input/index';
+import { UserContext } from '../../contexts/UserContext.jsx';
 
 export function LoginForm() {
   const username = useForm();
   const password = useForm();
 
-  useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token) {
-      getUser(token);
-    }
-  }, []);
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-  }
+  const { userLogin } = useContext(UserContext);
 
   async function handleAuthenticateUser(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      window.localStorage.setItem('token', json.token);
+      userLogin(username.value, password.value);
     }
   }
 
