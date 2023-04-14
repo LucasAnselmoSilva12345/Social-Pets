bff7c8fffdf6bd708f08e24092d5c408a93ebfe6
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-04-13 21:38:06 -0300

    Feat: Hook API Request
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/27
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/hooks/useAPIFetch.js
@@ -0,0 +1,36 @@
+import { useCallback, useState } from 'react';
+
+export const useAPIFetch = () => {
+  const [dataUser, setDataUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  const request = useCallback(async (url, options) => {
+    let response;
+    let json;
+
+    try {
+      setError(null);
+      setLoading(true);
+
+      response = await fetch(url, options);
+      json = await response.json();
+
+      if (!response.ok) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setDataUser(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return {
+    dataUser,
+    loading,
+    error,
+    request,
+  };
+};
