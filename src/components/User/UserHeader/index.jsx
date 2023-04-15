ea6942927b9fe0abbc9fccc31643331f9d04f922
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-04-14 21:30:43 -0300

    Feat: User Header Structure to all page associated
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/30
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/User/UserHeader/index.jsx
+++ src/components/User/UserHeader/index.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { UserHeaderNav } from '../UserHeaderNav';
+
+import style from './style.module.css';
+import { useLocation } from 'react-router-dom';
 
 export function UserHeader() {
+  const [userHeaderTitle, setUserHeaderTitle] = useState('');
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const { pathname } = location;
+    switch (pathname) {
+      case '/account/analytics':
+        setUserHeaderTitle('Analytics');
+        break;
+      case '/account/post':
+        setUserHeaderTitle('Post your photo');
+        break;
+      default:
+        setUserHeaderTitle('My Feed');
+    }
+  }, [location]);
+
   return (
-    <div>
-      <h4>User Header</h4>
-    </div>
+    <header className={style.userHeader}>
+      <h1 className="title">{userHeaderTitle}</h1>
+
+      <UserHeaderNav />
+    </header>
   );
 }
