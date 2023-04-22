c886430a2ea6562d3484fbc4a5ae868a1a1c3fee
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-04-22 19:10:56 -0300

    Feat Tailwind settings
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/35
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '8xl': '50rem',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        backgroundBase: '#FFFFFF',
+        colorTextBase: '#333333',
+        colorBlack: '#000000',
+        colorWhite: '#FFFFFF',
+        colorGray: '#F5F5F5',
+        shadowHeader: 'rgba(0, 0, 0, 0.1)',
+      },
+      zIndex: {
+        100: '100',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
