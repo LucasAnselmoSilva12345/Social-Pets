8cc37f75f5dcadeaa54e9ffe7ed0b58d9134aead
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-04-29 15:48:52 -0300

    Feat: Tailwind styles and rules in Footer component
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/35
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import { ReactComponent as DogsFooter } from '../../assets/dogs.svg';
 import { ThemeSwitcher } from '../ThemeSwitcher';
 
-import style from './style.module.css';
-
 export function Footer() {
   return (
-    <footer className={style.footer}>
-      <div className={`${style.wrapperFooter}`}>
+    <footer className='py-12 px-4 bg-colorYellow text-colorTextBase transition-all ease-in-out'>
+      <div className='max-w-8xl my-0 mx-auto py-0 px-1 flex items-center justify-between md:py-0 md:px-4'>
         <DogsFooter />
         <p>Social Pets. All rights reserved</p>
         <ThemeSwitcher />
