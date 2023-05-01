1f4d779d3dfbd4dc6b6e0daba148ce9a87da94f5
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-05-01 19:25:33 -0300

    Feat: Switch color text in Input component
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/37
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Forms/Input/index.jsx
+++ src/components/Forms/Input/index.jsx
@@ -11,8 +11,8 @@ export function Input({
   onBlur,
 }) {
   return (
-    <div className='mt-2 mr-0 mb-4 ml-0'>
-      <label htmlFor={id} className='block pb-2 text-base'>
+    <div className="mt-2 mr-0 mb-4 ml-0">
+      <label htmlFor={id} className="block pb-2 text-base">
         {label}
       </label>
       <input
@@ -23,9 +23,11 @@ export function Input({
         value={value}
         onChange={onChange}
         onBlur={onBlur}
-        className='w-full p-3 text-base border border-solid border-colorInput rounded-md bg-colorInput transition-all hover:outline-none hover:border-2 hover:border-solid hover:border-colorYellow focus:outline-none focus:border-2 focus:border-solid focus:border-colorYellow'
+        className="w-full p-3 text-base border border-solid border-colorInput rounded-md transition-all duration-300 bg-colorInput text-colorTextBase dark:bg-darkMedium dark:text-colorTextDark hover:outline-none hover:border-2 hover:border-solid hover:border-colorYellow focus:outline-none focus:border-2 focus:border-solid focus:border-colorYellow"
       />
-      {error && <p className='mt-1 text-sm font-bold text-colorError'>{error}</p>}
+      {error && (
+        <p className="mt-1 text-sm font-bold text-colorError">{error}</p>
+      )}
     </div>
   );
 }
