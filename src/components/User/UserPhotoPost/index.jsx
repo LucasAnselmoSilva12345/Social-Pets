451f6a1c8cc68a50d70c72ae9c362db94fe983cb
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-05-10 14:35:35 -0300

    Fixes: Form post photo styles
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/42
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/User/UserPhotoPost/index.jsx
+++ src/components/User/UserPhotoPost/index.jsx
@@ -42,7 +42,7 @@ export function UserPhotoPost() {
   }
 
   return (
-    <section className="grid grid-cols-2 gap-8 mb-8">
+    <section className="mb-4">
       <form onSubmit={handlePostPetPhoto}>
         <Input
           label="Names pet"
@@ -72,23 +72,25 @@ export function UserPhotoPost() {
           id="img"
           onChange={handleUpdateImgPhoto}
           required
-          className="mb-4"
+          className="relative m-0 mb-4 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-clip-padding px-3 py-[0.32rem] text-base font-normal text-neutral-700 transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-neutral-100 file:px-3 file:py-[0.32rem] file:text-neutral-700 file:transition file:duration-150 file:ease-in-out file:[border-inline-end-width:1px] file:[margin-inline-end:0.75rem] hover:file:bg-neutral-200 focus:border-primary focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:file:bg-neutral-700 dark:file:text-neutral-100 dark:focus:border-primary"
         />
+
+        {img.preview && (
+          <div className="mb-4">
+            <figure>
+              <img
+                src={`${img.preview}`}
+                alt=""
+                className="w-1/4 md:mt-6 md:mb-0 md:mx-auto md:flex md:items-center md:justify-center rounded-md"
+              />
+            </figure>
+          </div>
+        )}
 
         {loading ? <Button disabled>Loading...</Button> : <Button>Send</Button>}
 
         <Warning errorMessage={error} />
       </form>
-
-      <div>
-        {img.preview && (
-          <div>
-            <figure>
-              <img src={`${img.preview}`} alt="" />
-            </figure>
-          </div>
-        )}
-      </div>
     </section>
   );
 }
