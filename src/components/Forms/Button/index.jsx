892021caaeb0943b7bb9fab0748b2aa9164c397a
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-05-12 14:12:26 -0300

    Fixes: Button Size Component
    Issue: https://github.com/LucasAnselmoSilva12345/Social-Pets/issues/42
    
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/components/Forms/Button/index.jsx
+++ src/components/Forms/Button/index.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 export function Button({ children, ...props }) {
   return (
-    <button {...props} className='min-w-1/8 py-3 px-5 text-base font-medium font-inter border-none rounded-md bg-colorYellow text-colorBrownDark box-border transition-opacity ease-in-out cursor-pointer hover:opacity-70 focus:outline-none focus:shadow-shadowButton disabled:opacity-50 disabled:cursor-wait'>
+    <button
+      {...props}
+      className="md:min-w-1/8 min-w-full py-3 px-5 text-base font-medium font-inter border-none rounded-md bg-colorYellow text-colorBrownDark box-border transition-opacity ease-in-out cursor-pointer hover:opacity-70 focus:outline-none focus:shadow-shadowButton disabled:opacity-50 disabled:cursor-wait"
+    >
       {children}
     </button>
   );
