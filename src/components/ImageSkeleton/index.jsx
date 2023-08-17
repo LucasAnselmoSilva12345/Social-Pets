c7592e9f01cac3ab121a8361cfb3f0198a267656
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-08-17 15:20:59 -0300

    Feat: Image Skeleton
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/components/ImageSkeleton/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import style from './style.module.css';
+
+export function ImageSkeleton({ alt, ...props }) {
+  const [skeleton, setSkeleton] = useState(true);
+
+  function handleLoad({ target }) {
+    setSkeleton(false);
+    target.style.opacity = 1;
+  }
+
+  return (
+    <div className={style.wrapper}>
+      {skeleton && <div className={style.skeleton}></div>}
+
+      <img onLoad={handleLoad} className={style.img} alt={alt} {...props} />
+    </div>
+  );
+}
